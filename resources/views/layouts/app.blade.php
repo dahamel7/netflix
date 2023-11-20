8238c8635614a3d4a37220e723042b643805c338
naijel2103 <loick.michaud2103@gmail.com>
2023-11-20 14:21:18 -0500

    envoie layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('titre')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" style="text/css" href="../app.css">
     <script src="main.js"></script>
 </head>
@@ -29,26 +30,28 @@
       </div>      
       <nav class="main-nav">                
         <a href="http://127.0.0.1:8000/">Home</a>
-        <a href="#tvShows">TV Shows</a>
-        <a href="#movies">Movies</a>
+
         <a href="#originals">Originals</a>
         <a href="#">Recently Added</a>
         <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">Portfolio</a>        
         <a  href="http://127.0.0.1:8000/Personnes">Personnes</a>
+        @role('admin')
         <a href="{{route('administration.index')}}">Administration</a>
+        @endrole
       </nav>
       <nav class="sub-nav">
         <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>
         <a href="#"><i class="fas fa-bell sub-nav-logo"></i></a>
-        <a href="#">Account</a>        
+        <a href="/login">Login</a>  
+        <a href="/logout">Logout</a>           
       </nav>      
     </header>
 @yield('contenu')
 
 <!-- Mettre le footer -->
-  <footer>
-    <p>&copy 1997-2018 Netflix, Inc.</p>
-    <p>Carlos Avila &copy 2018</p>
-  </footer>
+<footer>
+      <p>&copy 1997-2018 Netflix, Inc.</p>
+      <p>Carlos Avila &copy 2018</p>
+    </footer>
 </body>
 </html>
